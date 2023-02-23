b2056ce36ef845f309a0de990528075a38597fec
kofikumi <kumiselorm12@gmail.com>
2023-02-23 02:37:50 +0000

    beating the deadline

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,20 @@
 #include "main.h"
-/*
-* main - entry point
-* Return: Always *
-*/
 
-int main (void)
+/**
+ * main - Prints Holberton as a message.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
-    return (*);
+	int str[] = {72, 111, 108, 98, 101, 114, 116, 111, 110};
+	int count, sz;
+
+	sz = sizeof(str) / sizeof(int);
+	for (count = 0; count < sz; count++)
+	{
+		_putchar(str[count]);
+	}
+	_putchar('\n');
+	return (0);
 }
